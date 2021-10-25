245433b2079229b29613503929d72903c0da8928
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-10-25 10:06:01 +0800

    [bi-home] 21. staggered list view首页卡片布局加载

--- lib/page/home_tab_page.dart
+++ lib/page/home_tab_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bili_talk/http/core/hi_error.dart';
+import 'package:flutter_bili_talk/http/dao/home_dao.dart';
 import 'package:flutter_bili_talk/model/home_model.dart';
+import 'package:flutter_bili_talk/model/video_model.dart';
+import 'package:flutter_bili_talk/util/toast.dart';
 import 'package:flutter_bili_talk/widget/hi_banner.dart';
+import 'package:flutter_bili_talk/widget/video_card.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 /// 首页顶部导航
 class HomeTabPage extends StatefulWidget {
@@ -16,21 +22,78 @@ class HomeTabPage extends StatefulWidget {
 }
 
 class _HomeTabPageState extends State<HomeTabPage> {
+  List<VideoModel> videoList = [];
+  int pageIndex = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MediaQuery.removePadding(
         // 移除顶部间距
         removeTop: true,
         context: context,
-        child: ListView(
-          children: [if (widget.bannerList != null) _banner()],
-        ));
+        child: StaggeredGridView.countBuilder(
+            padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+            crossAxisCount: 2,
+            itemCount: videoList.length,
+            itemBuilder: (BuildContext context, int index) {
+              //有banner时第一个item位置显示banner
+              if (widget.bannerList != null && index == 0) {
+                return Padding(
+                    padding: EdgeInsets.only(bottom: 8), child: _banner());
+              } else {
+                return VideoCard(
+                  videoModel: videoList[index],
+                );
+              }
+            },
+            staggeredTileBuilder: (int index) {
+              if (widget.bannerList != null && index == 0) {
+                return StaggeredTile.fit(2);
+              } else {
+                return StaggeredTile.fit(1);
+              }
+            }));
   }
 
   _banner() {
     return Padding(
-      padding: EdgeInsets.only(left: 8, right: 8),
+      padding: EdgeInsets.only(left: 5, right: 8),
       child: HiBanner(widget.bannerList),
     );
+  }
+
+  void _loadData({loadMore = false}) async {
+    if (!loadMore) {
+      pageIndex = 1;
+    }
+    var currentIndex = pageIndex + (loadMore ? 1 : 0);
+    try {
+      HomeMo result = await HomeDao.get(widget.categoryName,
+          pageIndex: currentIndex, pageSize: 50);
+      print('lHome oadData: $result');
+      setState(() {
+        if (loadMore) {
+          // 合并新的数据
+          videoList = [...videoList, ...result.videoList];
+          if (result.videoList.isNotEmpty) {
+            pageIndex++;
+          }
+        } else {
+          videoList = result.videoList;
+        }
+      });
+    } on NeedAuth catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    } on HiNetError catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    }
   }
 }
---
+++ lib/widget/video_card.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bili_talk/model/video_model.dart';
+import 'package:flutter_bili_talk/navigator/hi_navigator.dart';
+import 'package:flutter_bili_talk/util/format_util.dart';
+
+/// 视频卡片
+class VideoCard extends StatelessWidget {
+  final VideoModel videoModel;
+
+  const VideoCard({Key key, @required this.videoModel}) : super(key: key);
+
+  /// 图片
+  Widget _itemImage(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Image.network(
+          videoModel.cover ?? '',
+          width: size.width / 2 - 10,
+          height: 120,
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            padding: EdgeInsets.only(
+              left: 8,
+              right: 8,
+              bottom: 3,
+              top: 5,
+            ),
+            decoration: BoxDecoration(
+              // 渐变
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black54,
+                  Colors.transparent,
+                ],
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _iconText(Icons.ondemand_video, videoModel.view ?? 0),
+                _iconText(Icons.favorite_border, videoModel.favorite ?? 0),
+                _iconText(null, videoModel.duration ?? 0),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 图标文本
+  _iconText(IconData iconData, int count) {
+    String views = "";
+    if (iconData != null) {
+      views = countFormat(count);
+    } else {
+      views = durationTransform(videoModel.duration);
+    }
+    return Row(
+      children: [
+        if (iconData != null) Icon(iconData, color: Colors.white, size: 12),
+        Padding(
+          padding: EdgeInsets.only(left: 3),
+          child: Text(
+            views,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 信息文本
+  _infoText(Color textColor) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 5,
+          left: 8,
+          right: 8,
+          bottom: 5,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              videoModel.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 12,
+                color: textColor,
+              ),
+            ),
+            _owner(textColor)
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// 作者
+  _owner(Color textColor) {
+    var owner = videoModel.owner;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                owner.face,
+                height: 24,
+                width: 24,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Text(
+                owner.name,
+                style: TextStyle(
+                  fontSize: 11,
+                  color: textColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Icon(
+          Icons.more_vert_sharp,
+          size: 15,
+          color: Colors.grey,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color textColor = Colors.black87;
+    return InkWell(
+      onTap: () {
+        HiNavigator.getInstance().onJumpTo(
+          RouteStatus.detail,
+          args: {'videoModel': videoModel},
+        );
+      },
+      child: SizedBox(
+        height: 200,
+        child: Card(
+          // 取消卡片默认边距
+          margin: EdgeInsets.only(left: 4, right: 4, bottom: 8),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _itemImage(context),
+                _infoText(textColor),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
